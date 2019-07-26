34c36d0a898b9361a930b8ac8ff60bed50d128be
vebradev <justinas@vebra.dev>
2019-07-26 10:24:18 +0300

    Add users-model with functionality to add user

---
+++ config/users-model.js
@@ -0,0 +1,16 @@
+const db = require("./../database/dbConfig");
+
+module.exports = {
+  add
+};
+
+async function add(user) {
+  const [id] = await db("users").insert(user);
+  return findById(id);
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first();
+}
