eb47cf023aca3f151ebf1c999fb2e26e3ca08ade
vebradev <justinas@vebra.dev>
2019-07-26 10:45:13 +0300

    Add function to generate jwt to authenticate.js

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,12 +1,11 @@
 const jwt = require('jsonwebtoken');
 
-const jwtKey =
-  process.env.JWT_SECRET ||
-  'add a .env file to root of project with the JWT_SECRET variable';
+const jwtKey = process.env.JWT_SECRET
 
 // quickly see what this file exports
 module.exports = {
-  authenticate,
+  generateToken,
+  authenticate
 };
 
 // implementation details
@@ -26,4 +25,12 @@ function authenticate(req, res, next) {
       error: 'No token provided, must be set on the Authorization Header',
     });
   }
+}
+
+function generateToken(user) {
+  const payload = {
+    sub: user.id,
+  };
+
+  return jwt.sign(payload, process.env.JWT_SECRET);
 }
